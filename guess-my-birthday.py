6a9ccbaab05cfa85b72be51b952ca563453b8f57
Sean Kennedy <snkennedy21@gmail.com>
2022-07-07 16:51:06 -0400

    update spaces between lines

--- guess-my-birthday.py
+++ guess-my-birthday.py
@@ -9,32 +9,31 @@ name = input("Hi! What's your name: ")
 max_year = 2004
 min_year = 1924
 
-month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 
 i = 5
-while i >= 0:
+while i >= 2:
   year_guess = randint(min_year, max_year)
-  month_guess = randint(0, len(month_list))
+  month_guess = month_list[randint(0, len(month_list) - 1)]
 
   if i == 5:
-    player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
+    player_response = input(f'\n{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess}\n(y)es or (n)o: ')
 
   if player_response.lower() != 'n' and player_response.lower() != 'l':
     print('invalid input')
-    player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
+    player_response = input(f'\n{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess}\n(y)es or (n)o: ')
 
   if player_response.lower() == 'n':
     i = i - 1
     print('Drat! Let me try again!')
-    month_guess = randint(0, len(month_list))
+    month_guess = month_list[randint(0, len(month_list))]
     year_guess = randint(min_year, max_year)
-    player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
+    player_response = input(f'\n{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess}\n(y)es or (n)o: ')
 
   if player_response.lower() == 'y':
-    print('I knew it!')
+    print('\nI knew it!')
     break
 
   if i == 1 and player_response.lower() == 'n' :
-    print('I have more important things to do')
-    break
+    print('\nI have more important things to do')
