9e124f836dc65c6cc7e72c410cd3ba67d25ab587
Sean Kennedy <snkennedy21@gmail.com>
2022-07-07 13:21:35 -0400

    add invalid input error

--- guess-my-birthday.py
+++ guess-my-birthday.py
@@ -8,7 +8,6 @@ max_year = 2004
 min_year = 1924
 
 month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-message = "Drat! Let me try again!"
 
 
 i = 5
@@ -19,9 +18,14 @@ while i >= 0:
   if i == 5:
     player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
 
+  if player_response.lower() != 'n' and player_response.lower() != 'l':
+    print('invalid input')
+    player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
+
+
   if player_response.lower() == 'n':
     i = i - 1
-    print(f'{message}')
+    print('Drat! Let me try again!')
     month_guess = randint(0, len(month_list))
     year_guess = randint(min_year, max_year)
     player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
