d663d6868af0a3c4ccd4b4eb41e413b3bf13d5ec
Sean Kennedy <snkennedy21@gmail.com>
2022-07-07 13:17:24 -0400

    create-birthday-guesser

---
+++ guess-my-birthday.py
@@ -0,0 +1,36 @@
+from calendar import month
+import code
+from random import randint
+
+name = input("Hi! What's your name: ")
+
+max_year = 2004
+min_year = 1924
+
+month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+message = "Drat! Let me try again!"
+
+
+i = 5
+while i >= 0:
+  year_guess = randint(min_year, max_year)
+  month_guess = randint(0, len(month_list))
+
+  if i == 5:
+    player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
+
+  if player_response.lower() == 'n':
+    i = i - 1
+    print(f'{message}')
+    month_guess = randint(0, len(month_list))
+    year_guess = randint(min_year, max_year)
+    player_response = input(f'{i} Guesses Remaining: {name} were you born in {month_guess} / {year_guess} (y)es or (n)o: ')
+  if player_response.lower() == 'y':
+    print('I knew it!')
+    break
+
+  if i == 1:
+    print('I have more important things to do')
+    break
+
+
